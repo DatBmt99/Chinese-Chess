9c655c74fa96a0f24ebedb4eaa009623b31a262d
Trịnh Xuân Đạt <datbmtt1@gmail.com>
2021-04-04 09:31:13 +0700

    export item

---
+++ src/enums/PieceTypeZH.js
@@ -0,0 +1,11 @@
+import PieceType from "./PieceType";
+
+export default {
+    [PieceType.King]: "Tướng",
+    [PieceType.Rook]: "Xe",
+    [PieceType.Knight]: "Mã",
+    [PieceType.Bishop]: "Tượng",
+    [PieceType.Guard]: "Sĩ",
+    [PieceType.Cannon]: "Pháo",
+    [PieceType.Pawn]: "Tốt",
+}
