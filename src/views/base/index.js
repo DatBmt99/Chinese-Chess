79519da39d39e442d5a864e9c2350aedb691ec1f
Pham Duy <phd051199@gmail.com>
2021-04-08 14:08:09 +0700

    Base index close action

--- src/views/base/index.js
+++ src/views/base/index.js
@@ -2,25 +2,34 @@ import React from "react";
 import WebSocketClient from "../../utils/WebSocketClient";
 import Global from "../../utils/Global";
 import StoreKey from "../../utils/StoreKey";
-import {Views} from "../../enums/index";
+import { Views } from "../../enums/index";
 
-export default class BaseComponent extends React.Component{
-    constructor(props){
+export default class BaseComponent extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    checkLogin(){
-        if (!sessionStorage.getItem(StoreKey.uuid)){
+    checkLogin() {
+        if (!sessionStorage.getItem(StoreKey.uuid)) {
             this.props.history.push(Views.index.pathname);
         }
-        if (!Global.socket){
+        if (!Global.socket) {
             Global.socket = new WebSocketClient({
                 uuid: sessionStorage.getItem(StoreKey.uuid),
                 user: sessionStorage.getItem(StoreKey.user),
-                fallback: ()=>{
+                fallback: () => {
                     this.props.history.push(Views.index);
                 }
             });
+        }
+        if (!window.onunload) {
+            window.onunload = function () {
+                Global.socket.send({
+                    action: "close",
+                    uuid: sessionStorage.getItem(StoreKey.uuid),
+                    user: sessionStorage.getItem(StoreKey.user),
+                });
+            };
         }
     }
 }
