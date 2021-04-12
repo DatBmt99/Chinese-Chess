a4251d2993ecd3bd8cc6a16b08390d24f380c70f
HieuSoi <dinhhoanghieu321@gmail.com>
2021-04-12 20:14:17 +0700

    update players

--- src/views/main/players/index.js
+++ src/views/main/players/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import BaseComponent from "../../base/index";
 import { PieceColor, PlayerStatus } from "../../../enums/index";
-import { Global } from "../../../utils";
-import StoreKey from "../../../utils/StoreKey";
 
 export default class Players extends BaseComponent {
     static defaultProps = {
